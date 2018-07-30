ccd17f96df4a9d39576c8b1403306870587fbd81
Tim Sullivan <timsullivan97@gmail.com>
2018-07-29 20:03:42 -0700

    fixing line spacing to be exact with code from book

--- ex5.rb
+++ ex5.rb
@@ -10,14 +10,12 @@ inches_to_centimeters = height * 2.54
 # 0.453592 is equal to 1 pound. This variable pounds_to_kilograms will multiply the value set equal to the weight variable and give of a value of 81.64656
 pounds_to_kilograms = weight * 0.453592
 
-
 puts "Let's talk about #{name}."
 puts "He's #{inches_to_centimeters} centimeters tall."
 puts "He's #{pounds_to_kilograms} kilgorams heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the on the coffee."
-
 
 # this line is tricky, try to get it exactly right
 puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
