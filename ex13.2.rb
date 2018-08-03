51c61ec520492da7a8da5a8d35022b110116aa3e
Tim Sullivan <timsullivan97@gmail.com>
2018-08-02 20:37:00 -0700

    tinkering with ARGV exercise 13

---
+++ ex13.2.rb
@@ -0,0 +1,12 @@
+house, food, location, mansion = ARGV.clear
+
+puts "Do you live in a #{house}?"
+user_house_input = gets.chomp
+
+puts "Have you had a #{food} before?"
+user_food_input = gets.chomp
+
+puts "\n\tHave you eaten a in a #{location} before?"
+
+puts "Food in a #{mansion} makes it that much better."
+user_mansion_input = gets.chomp
