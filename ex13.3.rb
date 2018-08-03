daf83bdb354d7a32ccc389886438cc60507913a0
Tim Sullivan <timsullivan97@gmail.com>
2018-08-02 21:00:15 -0700

    updaitng ARGV exercise file

---
+++ ex13.3.rb
@@ -0,0 +1,6 @@
+# Creating an array that is set equal to ARGV
+input_array = ARGV
+# Depending on how many arguments added to the array in the command line this statement will display the length of the array
+puts input_array.length
+# This will convert the input_array to a string and display the array when run
+puts input_array.to_s
