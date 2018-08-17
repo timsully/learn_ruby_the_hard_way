8f3045d545942c6e46ee7c3c04376334ee961643
Tim Sullivan <timsullivan97@gmail.com>
2018-08-16 22:35:10 -0700

    adding comments

--- ex33.rb
+++ ex33.rb
@@ -1,10 +1,21 @@
+# Assigning i equal to 0
 i = 0
+# Assigning numbers equal to an empty array to push in values
+# that will be stored as elements in the array and be able to
+# target them individuallt via index
 numbers = []
 
+# while i is less than 6 execute its respective code block
 while i < 6
+  # Outputs a string and the variable i which increments
+  # by 1 each time the while loop restarts
   puts "At the top i is #{i}"
+  # Accessing the numbers array within the code block
+  # to use the push method to add the value of i into the array
   numbers.push(i)
+  # Increment by 1 each time it loops until the expression is false
   i += 1
+  # Outputs the string with the array and how far it has incremented
   puts "Numbers now: ", numbers
   puts "At the bottom i is #{i}"
 end
