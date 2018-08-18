86a551c607e89e9aea7ee76c3f34c9da351fcd7e
Tim Sullivan <timsullivan97@gmail.com>
2018-08-17 21:20:11 -0700

    re-ordering functions to better understand program

--- ex35.rb
+++ ex35.rb
@@ -1,24 +1,19 @@
-def gold_room
-  puts "This room is full of gold. How much do you take?"
+def start
+  puts "You are in a dark room."
+  puts "There is a door to your right and left."
+  puts "Which one do you take?"
 
   print "> "
   choice = $stdin.gets.chomp
 
-  # this line has a bug, so fix it
-  if choice.include? == "0" || choice.include? == "1"
-    how_much = choice.to_i
+  if choice == "left"
+    bear_room
+  elsif choice == "right"
+    cthulhu_room
   else
-    dead("Man, learn to type a number.")
-  end
-
-  if how_much < 50
-    puts "Nice, you're not greedy, you win!"
-    exit(0)
-  else
-    dead("You greedy bastard!")
+    dead("You stumble around the room until you starve.")
   end
 end
-
 
 def bear_room
   puts "There is a bear here."
@@ -65,27 +60,32 @@ def cthulhu_room
 end
 
 
+def gold_room
+  puts "This room is full of gold. How much do you take?"
+
+  print "> "
+  choice = $stdin.gets.chomp
+
+  # this line has a bug, so fix it
+  if choice.include? == "0" || choice.include? == "1"
+    how_much = choice.to_i
+  else
+    dead("Man, learn to type a number.")
+  end
+
+  if how_much < 50
+    puts "Nice, you're not greedy, you win!"
+    exit(0)
+  else
+    dead("You greedy bastard!")
+  end
+end
+
+
 def dead(why)
   puts why, "Good job!"
   exit(0)
 end
 
-
-def start
-  puts "You are in a dark room."
-  puts "There is a door to your right and left."
-  puts "Which one do you take?"
-
-  print "> "
-  choice = $stdin.gets.chomp
-
-  if choice == "left"
-    bear_room
-  elsif choice == "right"
-  cthulhu_room
-  else
-    dead("You stumble around the room until you starve.")
-  end
-end
 
 start
