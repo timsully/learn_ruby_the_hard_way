3c4fb4f45965268eb055870a7741c7bdef90cd81
Tim Sullivan <timsullivan97@gmail.com>
2018-08-17 20:30:34 -0700

    updating README.md and adding exercise

---
+++ ex34.rb
@@ -0,0 +1,14 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+tiger = animals[1]
+penguin = animals[2]
+zebra = animals[3]
+
+
+puts "Index 0 holds this string: #{bear}"
+
+puts "Index 1 holds this string: #{tiger}"
+
+puts "Index 2 holds this string: #{penguin}"
+
+puts "Index 3 holds this string: #{zebra}"
