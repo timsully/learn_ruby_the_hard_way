ac3b51a348b6b3e85326a72c0f764537bad229f6
Tim Sullivan <tim.sullivan97@gmail.com>
2018-08-21 20:57:16 -0700

    adding comments in source file to guide/outline my program

---
+++ ex36.rb
@@ -0,0 +1,43 @@
+# The first step would be to turn on your coomputer
+
+
+# Activate your preferred text editor via terminal hehe
+
+
+# Create a new file and open it in your preferred text editor
+
+
+# If 'touch test.txt' was typed in terminal then it is correct
+
+
+# Then open test.txt with your preferred text editor with:  <application> test.txt
+
+
+# Press Y or N to finish writing file which will save or close it
+
+
+# Then open back up terminal
+
+
+# Prompt the user to ask what is a way of uploading to github via terminal?
+
+
+# First step, change directory to file, example could be `cd ~/desktop/test.txt'
+
+
+# Question user with three options, A being do git status or B, git add <filename>.txt or C, git branch ???
+# Use a conditional statement
+
+
+# If A then go to check branch, if B 'try again', if C 'try again'
+# Possibly hint at user the solution if failed to many times? (challenge)
+
+
+# After adding ask user the proper syntax for committing, else prompt user to do research on how to do so at
+# the actual URL of Github docs
+
+
+# When everything is correct git push
+
+
+# Once that is done prompt user with a completion
