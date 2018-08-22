9630de76323415477c0325eccf4f04565d36f580
Tim Sullivan <timsullivan97@gmail.com>
2018-08-21 21:39:46 -0700

    adding second chunk of program

--- ex36.rb
+++ ex36.rb
@@ -1,5 +1,4 @@
-# The first step would be to turn on your coomputer
-
+def start
   puts '''Welcome, you have now entered Tim\'s first ever program where he actually
   implemented his thought and wrote out his actions on a piece of paper to write
   out a so called program thanks to Learn Ruby the Hard Way by Zed Shaw. As a Noob
@@ -14,12 +13,36 @@
   too_much_text = $stdin.gets.chomp
 
   if too_much_text == "yes" || too_much_text == "no"
-    puts "You are damn right!"
+    puts "You are damn right! Now let's begin."
+    second_step
   else
-    dead("Thank you, come again with a yes or a no.")
+    puts "Come again with a yes or a no. Bye Felicia!"
+    exit(0)
+  end
+end
+
+
+# The first step would be to turn on your computer
+def second_step
+  puts "If you don't get this right then I don't know what to tell ya."
+  puts "What is the first step we need to execute in order to upload a file to our Github repo?\n"
+  puts "1. Turn on computer?"
+  puts "2. Change into the proper directory?"
+
+  print "> "
+  second_input = $stdin.gets.chomp
+
+  if second_input == "1" || second_input == "Turn on computer"
+    puts "You are correct. Having your computer ON is a must."
+    # call the next function
+  else
+    puts "I dont\'t know what to tell ya."
+    exit(0)
   end
 
+end
 
+start
 # Activate your preferred text editor via terminal hehe
 
 
