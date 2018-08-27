b4898c2d41ae8885480bd08a0b2bdf95d6f9707d
Tim Sullivan <timsullivan97@gmail.com>
2018-08-26 22:18:37 -0700

    adding test file

---
+++ ex39_test.rb
@@ -0,0 +1,51 @@
+require './dict.rb'
+
+# creates a mapping of state to abbreviation
+states = Dict.new()
+Dict.set(states, 'Oregon', 'OR')
+Dict.set(states, 'Flordia', 'FL')
+Dict.set(states, 'California', 'CA')
+Dict.set(states, 'New York', 'NY')
+Dict.set(states, 'Michigan', 'MI')
+
+# create a basic set of states and some cities in them
+cities = Dict.new()
+Dict.set(cities, 'CA', San Franisco)
+Dict.set(cities, 'MI', 'Detroit')
+Dict.set(cities, 'FL', 'Jacksonville')
+
+# add some more cities
+Dict.set(cities, 'NY', 'New York')
+Dict.set(cities, 'OR', 'Portland')
+
+
+# puts out some cities
+puts '-' * 10
+puts "NY State has: #{Dict.get(cities, 'NY')}"
+puts "OR State has: #{Dict.get(cities, 'OR')}"
+
+# puts some states
+puts '-' * 10
+puts "Michigan's abbreviaton is: #{Dict.get(states, 'Michigan')}"
+puts "Florida's abbreviaton is: #{Dict.get(states, 'Florida')}"
+
+# do it by using the state then cities dict
+puts '-' * 10
+puts "Michigan has: #{Dict.get(cities, Dict.get(states, 'Michigan'))}"
+puts "Florida has: #{Dict.get(cities, Dict.get(states, 'Florida'))}"
+
+# puts every state abbreviation
+puts '-' * 10
+Dict.list(cities)
+
+puts '-' * 10
+# by default ruby says "nil" when something isn't there
+state = Dict.get(state, 'Texas')
+
+if !state
+  puts "Sorry, no Texas."
+end
+
+# default values using ||= with nil result
+city = Dict.get(cities, 'TX', 'Does Not Exist')
+puts "The city for the state 'TX' is: #{city}"
