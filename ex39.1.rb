c7d044a0d43592cfc45b0e8091650a592b88124c
Tim Sullivan <timsullivan97@gmail.com>
2018-08-26 21:24:14 -0700

    adding exercise 39 files

---
+++ ex39.1.rb
@@ -0,0 +1,43 @@
+# IRB Practice From Exercise 39 Ruby Session
+things = ['a', 'b', 'c', 'd']
+
+puts things[1]
+
+things[1] = 'z'
+
+puts things[1]
+
+things
+
+# Second Exercise 39 Ruby Session
+stuff = {'name' => 'Zed', 'age' => 39, 'height' => 6 * 12 + 2}
+
+puts stuff['name']
+
+puts stuff['age']
+
+puts stuff['height']
+
+stuff['city'] = "San Francisco"
+
+print stuff['city']
+
+# Third Exercise 39 Ruby Session
+stuff[1] = "Wow"
+
+stuff[2] = "Neato"
+
+puts stuff[1]
+
+puts stuff[2]
+
+stuff
+
+# Fourth Exercise 39 Ruby Session
+stuff.delete('city')
+
+stuff.delete(1)
+
+stuff.delete(2)
+
+stuff
