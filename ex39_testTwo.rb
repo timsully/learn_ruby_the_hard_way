eb00d39a92d356b1734b78457650196810d819cd
Tim Sullivan <timsullivan97@gmail.com>
2018-09-01 22:50:52 -0700

    exercise: mapping w/ hashes

--- ex39_testTwo.rb
+++ ex39_testTwo.rb
@@ -1,1 +1,19 @@
 require './dict.rb'
+
+# creates a mapping of state to abbreviation
+states = Dict.new()
+Dict.set(states, 'Jalisco', 'Jal.')
+Dict.set(states, 'Morelos', 'Mor.')
+Dict.set(states, 'Nayarit', 'Nay.')
+Dict.set(states, 'Oaxaca', 'Oax.')
+Dict.set(states, 'Sinaloa', 'Sin.')
+
+# creates a basic set of states and some cities in them
+cities = Dict.new()
+Dict.set(cities, 'Jal.', 'Puerto Vallarta')
+Dict.set(cities, 'Oax.', 'Salina Cruz')
+Dict.set(cities, 'Sin.', 'Culiacan')
+
+# add some more cities
+Dict.set(cities, 'Jal.', 'El Tule')
+Dict.set(cities, 'Sin.', 'Mazatlan')
