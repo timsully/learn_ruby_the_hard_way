185f9df30e270229c3c54bfef4fbce4446abb24c
Tim Sullivan <timsullivan97@gmail.com>
2018-09-09 15:30:06 -0700

    updating file with comments to discuss relationship between classes created and object instantiated

--- ex42.rb
+++ ex42.rb
@@ -2,29 +2,29 @@
 class Animal
 end
 
-## ??
+## is-a
 class Dog < Animal
 
     def initialize(name)
-        ## ?? 
+        ## has-a 
         @name = name
     end
 end
 
-## ?? 
+## is-a 
 class Cat < Animal
 
     def initialize(name)
-        ## ??
+        ## has-a
         @name = name
     end
 end
 
-## ??
+## is-a
 class Person
 
     def initialize(name)
-        ## ?? 
+        ## has-a 
         @name = name
 
         ## Person has-a pet of some kind
@@ -34,27 +34,27 @@ class Person
     attr_accessor :pet ## dafuq dis?
 end
 
-## ??
+## is-a
 class Employee < Person
 
     def initialize(name, salary)
         ## ?? hmm what is this strange magic?
         super(name)
-        ## ??
+        ## has-a
         @salary = salary
     end
 
 end
 
-## ??
+## is-a
 class Fish
 end
 
-## ?? 
+## is-a 
 class Salmon < Fish
 end
 
-## ??
+## is-a
 class Halibut < Fish
 end
 
@@ -62,26 +62,26 @@ end
 ## rover is-a Dog
 rover = Dog.new("Rover")
 
-## ?? 
+## satan is-a Cat 
 satan = Cat.new("Satan")
 
-## ??
+## mary is-a Person
 mary = Person.new("Mary")
 
-## ?? 
+## mary has-a pet named satan 
 mary.pet = satan
 
-## ??
+## frank is-a Employee
 frank = Employee.new("Frank", 120000)
 
-## ??
+## frank has-a pet named rover
 frank.pet = rover
 
-## ??
+## flipper is-a Fish
 flipper = Fish.new()
 
-## ??
+## crouse is-a Salmon
 crouse = Salmon.new()
 
-## ??
+## harry is a Halibut
 harry = Halibut.new()
