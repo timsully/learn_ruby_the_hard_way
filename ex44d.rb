e92c4d6a2ffaa543ce6eb86be0b9b97f250577bb
Tim Sullivan <timsullivan97@gmail.com>
2018-10-29 21:02:38 -0700

    updating exercise file w/ comments to understand functions being called from classes

--- ex44d.rb
+++ ex44d.rb
@@ -44,15 +44,34 @@ class Child < Parent
 end
 
 # A new instance of the Parent class is assigned to the variable dad
-# 
 dad = Parent.new()
+# An instance of the Child class is assigned to the variable son
 son = Child.new()
 
+# Calling the Parent.implicit function which results in displaying
+# PARENT implicit()
 dad.implicit()
+# Calling the Child.implicit(), BUT there is no implicit() function
+# under the Child class, ruby then goes and looks for it in the Parent
+# class since Child is one level under it in the hierarchy and ruby
+# find the implicit() function in the Parent class and displays the
+# output of PARENT implicit()
 son.implicit()
 
+# Calling override on the dad variable which is an instance of the
+# Parent class where it find and run the function override() which
+# will display PARENT override()
 dad.override()
+# Calling override function from the new Child class instance created
+# that is stored in the variable 'son' which will display CHILD override()
 son.override()
 
+# Calls the altered function from the Parent class resulting in the
+# output being: PARENT altered()
 dad.altered()
+# Calling the altered function stored in the Child class on the
+# variable instance son which will display the following:
+# CHILD, BEFORE PARENT altered() > then calls the Parent classes
+# altered() functions to get its output which is PARENT altered() >
+# then runs the last puts statement which is: CHILD, AFTER PARENT altered()
 son.altered()
