542668919c8734479f6d1f20cddb085c052054a1
Tim Sullivan <timsullivan97@gmail.com>
2018-11-15 21:00:55 -0800

    fixing path and creating Test Case

--- ex47/tests/test_ex47.rb
+++ ex47/tests/test_ex47.rb
@@ -1,10 +1,12 @@
-require "./lib/NAME.rb"
+require "ex47/game.rb"
 require "test/unit"
 
-class TestNAME < Test::Unit::TestCase
+class TestEx47 < Test::Unit::TestCase
 
-    def test_sample
-        assert_equal(4, 2+2)
+    def test_room()
+        gold = Room.new("Gold Room", """This room has gold in it you can grad. There's a door to the north.""")
+        assert_equal(gold.name, "Gold Room")
+        assert_equal(gold.paths, {})
     end
     
 end
