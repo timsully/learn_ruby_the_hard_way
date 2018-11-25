2457b61228afa7ed20358200fd85b94f0d6e5b3a
Tim Sullivan <timsullivan97@gmail.com>
2018-11-25 15:15:25 -0800

    add comments to describe conditional statement

--- ex48/lexicon.rb
+++ ex48/lexicon.rb
@@ -22,6 +22,8 @@ end
 class Parser
     # parser will take word_list and returns string or nil
     def self.peek(words_list)
+        # if word_list is not empty, it looks at the very first
+        # word element and returns the word type
         if words_list
             word = words_list[0]
             return word[0]
